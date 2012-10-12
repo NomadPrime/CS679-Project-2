b4b9a789677aabbdf52eed03bd90e77ceab08beb
unknown <eggum@cyclops-04.ad.cs.wisc.edu>
2012-10-11 23:21:13 -0500

    Changed to be a rotating, colored cube

--- adam_js.js
+++ adam_js.js
@@ -1,13 +1,25 @@
 var canvas;
 var gl;
-var squareVerticesBuffer;
-var squareVerticesColorBuffer;
+
+var cubeVerticesBuffer;
+var cubeVerticesColorBuffer;
+var cubeVerticesIndexBuffer;
+var cubeRotation = 0.0;
+var cubeXOffset = 0.0;
+var cubeYOffset = 0.0;
+var cubeZOffset = 0.0;
+var lastCubeUpdateTime = 0;
+var xIncValue = 0.2;
+var yIncValue = -0.4;
+var zIncValue = 0.3;
+
 var mvMatrix;
 var shaderProgram;
 var vertexPositionAttribute;
 var vertexColorAttribute;
 var perspectiveMatrix;
-var squareRotation = 0.0;
+
+
 
 function start() {
 	canvas = document.getElementById("glCanvas");
@@ -36,6 +48,10 @@ function start() {
 }
 
 
+/**
+ * Initialize WebGL, returning the GL context or null if WebGL is not
+ * available or could not be initialized 
+ */
 function initWebGL(canvas) {
 	// Initialize the global variable gl to null
 	gl = null;
@@ -53,20 +69,20 @@ function initWebGL(canvas) {
 	}
 }
 
-
+/**
+ * Initialize the shaders so WebGl knows how to light our scene. 
+ */
 function initShaders() {
 	var fragmentShader = getShader(gl, "shader-fs");
 	var vertexShader = getShader(gl, "shader-vs");
 	
 	// Create the shader program
-	
 	shaderProgram = gl.createProgram();
 	gl.attachShader(shaderProgram, vertexShader);
 	gl.attachShader(shaderProgram, fragmentShader);
 	gl.linkProgram(shaderProgram);
 	
-	// If creating the shader program worked, alert
-	
+	// If creating the shader program failed, alert
 	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
 		alert("Unable to initialize shader progarm");
 	}
@@ -81,15 +97,21 @@ function initShaders() {
 }
 
 
+/**
+ * Loads a shader program by scouring the current document, loking for a 
+ * script with the specified ID. 
+ */
 function getShader(gl, id) {
 	var shaderScript, theSource, currentChild, shader;
 	
 	shaderScript = document.getElementById(id);
 	
+	// If could not find element with given ID, abort
 	if (!shaderScript) {
 		return null;
 	}
 	
+	// Walk through source element's children to build source string
 	theSource = "";
 	currentChild = shaderScript.firstChild;
 	
@@ -101,6 +123,7 @@ function getShader(gl, id) {
 		currentChild = currentChild.nextSibling;
 	}
 	
+	// Figure out what type of shader script we have, based on MIME type
 	if (shaderScript.type == "x-shader/x-fragment") {
 		shader = gl.createShader(gl.FRAGMENT_SHADER);
 	} else if (shaderScript.type == "x-shader/x-vertex") {
@@ -110,6 +133,7 @@ function getShader(gl, id) {
 		return null;
 	}
 	
+	// Send the source to the shader object
 	gl.shaderSource(shader, theSource);
 	
 	// Compile the shader program
@@ -127,29 +151,103 @@ function getShader(gl, id) {
 
 var horizAspect = 480.0/640.0;
 
+/**
+ * Initialize the buffers we'll need.  For now, just have one object.
+ * It's a simple two-dimensional square 
+ */
 function initBuffers() {
-	squareVerticesBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
 	
+	// Create buffer for the square's vertices
+	cubeVerticesBuffer = gl.createBuffer();
+	
+	// Select the squareVerticesBuffer as one to apply vertex operations to 
+	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesBuffer);
+	
+	// Array of vertices for the square.  Note: z is always 0 here.
 	var vertices = [
-		1.0, 1.0, 0.0,
-		-1.0, 1.0, 0.0,
-		1.0, -1.0, 0.0,
-		-1.0, -1.0, 0.0
+		// Front face
+	  	-1.0, -1.0,  1.0,
+	   	1.0, -1.0,  1.0,
+	  	1.0,  1.0,  1.0,
+	  	-1.0,  1.0,  1.0,
+	   
+	  	// Back face
+	  	-1.0, -1.0, -1.0,
+	  	-1.0,  1.0, -1.0,
+	   	1.0,  1.0, -1.0,
+	   	1.0, -1.0, -1.0,
+	   
+	  	// Top face
+	  	-1.0,  1.0, -1.0,
+	  	-1.0,  1.0,  1.0,
+	   	1.0,  1.0,  1.0,
+	   	1.0,  1.0, -1.0,
+	   
+	  	// Bottom face
+	  	-1.0, -1.0, -1.0,
+	   	1.0, -1.0, -1.0,
+	   	1.0, -1.0,  1.0,
+	  	-1.0, -1.0,  1.0,
+	   
+	  	// Right face
+	   	1.0, -1.0, -1.0,
+	   	1.0,  1.0, -1.0,
+	   	1.0,  1.0,  1.0,
+	   	1.0, -1.0,  1.0,
+	   
+	  	// Left face
+	  	-1.0, -1.0, -1.0,
+	  	-1.0, -1.0,  1.0,
+	  	-1.0,  1.0,  1.0,
+	  	-1.0,  1.0, -1.0
 	];
 	
 	// Pass in the list of vertices into WebGL to build the shape.
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	
+	// Set up the colors for the vertices
 	var colors = [
-		1.0, 1.0, 1.0, 1.0,		//white
-		1.0, 0.0, 0.0, 1.0,		//red
-		0.0, 1.0, 0.0, 1.0,		//green
-		0.0, 0.0, 1.0, 1.0		//blue
+		[1.0, 1.0, 1.0, 1.0],			// Front face: white
+		[1.0, 0.0, 0.0, 1.0],			// Back face: red
+		[0.0, 1.0, 0.0, 1.0],			// Top face: green
+		[0.0, 0.0, 1.0, 1.0],			// Bottom face: blue
+		[1.0, 1.0, 0.0, 1.0],			// Right face: yellow
+		[1.0, 0.0, 1.0, 1.0]			// Left face: purple
 	];
 	
-	squareVerticesColorBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesColorBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+	// Convert the array of colors into a table for all of the vertices
+	var generatedColors = [];
+	
+	for (j = 0; j < 6; j++) {
+		var c = colors[j];
+		
+		// Repeat each color four times for the four vertices of the face
+		for (var i = 0; i < 4; i++) {
+			generatedColors = generatedColors.concat(c);
+		}
+	}
+	
+	cubeVerticesColorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesColorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(generatedColors), gl.STATIC_DRAW);
+	
+	
+	// Build the element array buffer
+	cubeVerticesIndexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVerticesIndexBuffer);
+	
+	// This array defines each face as two triangles, specifying each triangle's position
+	var cubeVertexIndices = [
+		0,  1,  2, 		0, 	2, 	3,			//front
+		4,  5,  6, 		4, 	6, 	7,			//back 
+		8,  9,  10, 	8, 	10, 11,			//top
+		12, 13, 14, 	12, 14, 15,			//bottom
+		16, 17, 18, 	16, 18, 19,			//right
+		20, 21, 22, 	20, 22, 23			//left
+	];
+	
+	// Now send the element array to GL
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
 }
 
 
@@ -167,18 +265,51 @@ function drawScene() {
 	//Back the drawing position up a little bit so that we can see it
 	mvTranslate([-0.0, 0.0, -6.0]);
 	
-	// Draw the square - bind array buffer and push to GL
-	gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
-	gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+	// Save the current matrix, then rotate before we draw
+	mvPushMatrix();	
 	
-	// Set the colors attribute for the vertices
-	gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesColorBuffer);
-	gl.vertexAttribPointer(vertexColorAttribute, 4, gl.FLOAT, false, 0, 0);
+		//rotate around the X and Z axes
+		mvRotate(cubeRotation, [1, 0, 1]);
+		// mvTranslate([cubeXOffset, cubeYOffset, cubeZOffset]);
 	
-	// Draw the square
-	setMatrixUniforms();
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+		// Draw the cube - bind array buffer and push to GL
+		gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesBuffer);
+		gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+		
+		// Set the cube attribute for the vertices
+		gl.bindBuffer(gl.ARRAY_BUFFER, cubeVerticesColorBuffer);
+		gl.vertexAttribPointer(vertexColorAttribute, 4, gl.FLOAT, false, 0, 0);
+		
+		// Draw the cube
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVerticesIndexBuffer);
+		setMatrixUniforms();
+		gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+	
+	mvPopMatrix();
+	
+	//determines how far to rotate the square based off of time since last rotation
+	var currentTime = (new Date).getTime();
+	if (lastCubeUpdateTime) {
+		var delta = currentTime - lastCubeUpdateTime;
+		cubeRotation += (30 * delta) / 1000.0;
+		
+		cubeXOffset += xIncValue * ((30 * delta) / 1000.0);
+		cubeYOffset += yIncValue * ((30 * delta) / 1000.0);
+		cubeZOffset += zIncValue = ((30 * delta) / 1000.0);
+		
+		if (Math.abs(cubeYOffset) > 2.5) {
+			xIncValue = -xIncValue;
+			yIncValue = -yIncValue;
+			zIncValue = -zIncValue;
+		}
+	}
+	
+	lastCubeUpdateTime = currentTime;
 }
+
+/**
+ * Matrix utility functions 
+ */
 
 function loadIdentity() {
 	mvMatrix = Matrix.I(4);
@@ -198,4 +329,32 @@ function setMatrixUniforms() {
  
   var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
   gl.uniformMatrix4fv(mvUniform, false, new Float32Array(mvMatrix.flatten()));
+}
+
+var mvMatrixStack = [];
+
+function mvPushMatrix(m) {
+	if (m) {
+		mvMatrixStack.push(m.dup());
+		mvMatrix = m.dup();
+	} 
+	else {
+		mvMatrixStack.push(mvMatrix.dup());
+	}
+}
+
+function mvPopMatrix() {
+	if (!mvMatrixStack.length) {
+		throw("Can't pop from an empty matrix stack.");
+	}
+	
+	mvMatrix = mvMatrixStack.pop();
+	return mvMatrix;
+}
+
+function mvRotate(angle, v) {
+	var inRadians = angle * Math.PI / 180.0;
+	
+	var m = Matrix.Rotation(inRadians, $V([v[0], v[1], v[2]])).ensure4x4();
+	multMatrix(m);
 }
